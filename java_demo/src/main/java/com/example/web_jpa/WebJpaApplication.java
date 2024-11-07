86fb30ab68b49ed46e0feecc513c1997706fb8d3
xiaorui-069 <1239058750@qq.com>
2024-11-07 15:48:32 -0500

    Update WebJpaApplication.java

--- java_demo/src/main/java/com/example/web_jpa/WebJpaApplication.java
+++ java_demo/src/main/java/com/example/web_jpa/WebJpaApplication.java
@@ -9,7 +9,7 @@ public class WebJpaApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(WebJpaApplication.class, args);
-        System.out.println("Hello World!");
+        System.out.println("The hooker is available at Jenkins.!");
     }
 
 }
